a2f6ecda5cbe7d3d20cc4eb2c6d74d8d8698ee1b
Yaroslav Benin <dondublon1@yandex.ru>
2026-07-02 10:30:25 +0300

    App.tsx & main.tsx ti chakra.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,18 @@
+import { Container } from '@chakra-ui/react'
 import { Routes, Route } from 'react-router-dom'
+
 import { SuperMenuList } from './components/SuperMenuList'
 import { MenuEditPanel } from './components/MenuEditPanel'
 
 function App() {
     return (
-        <div style={{ maxWidth: 800, margin: '0 auto', padding: '20px' }}>
+        <Container py={3}>
             <Routes>
                 <Route path="/" element={<SuperMenuList />} />
                 <Route path="/new" element={<MenuEditPanel />} />
                 <Route path="/edit/:menuItemId" element={<MenuEditPanel />} />
             </Routes>
-        </div>
+        </Container>
     )
 }
 
